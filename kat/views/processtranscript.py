4176f7139dd542110277d93cf41cba890ca77822
legeorges <georges.labreche@gmail.com>
2014-10-01 11:55:11 -0700

    Fixin'

--- kat/views/processtranscript.py
+++ kat/views/processtranscript.py
@@ -2,7 +2,7 @@ from flask import render_template
 from flask.views import MethodView
 
 
-class TranscriptionFormInterface(MethodView):
+class TranscriptProcessor(MethodView):
 
     methods = ['GET','POST']
 
